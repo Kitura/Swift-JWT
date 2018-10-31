17c5b0d44c0330f2ad43238eeba70f97865c88b0
andy <andrew.lees11@ibm.com>
2018-10-31 16:53:32 +0000

    update README

--- Sources/SwiftJWT/Claims.swift
+++ Sources/SwiftJWT/Claims.swift
@@ -21,34 +21,6 @@ import Foundation
 public protocol Claims: Codable {
     
     /**
-     The "iss" (issuer) claim identifies the principal that issued the
-     JWT.  The processing of this claim is generally application specific.
-     The "iss" value is a case-sensitive.
-     */
-    var iss: String? { get }
-    
-    /**
-     The "sub" (subject) claim identifies the principal that is the
-     subject of the JWT.  The claims in a JWT are normally statements
-     about the subject.  The subject value MUST either be scoped to be
-     locally unique in the context of the issuer or be globally unique.
-     The processing of this claim is generally application specific.  The
-     "sub" value is case-sensitive.
-     */
-    var sub: String? { get }
-    
-    /**
-     The "aud" (audience) claim identifies the recipients that the JWT is
-     intended for.  Each principal intended to process the JWT MUST
-     identify itself with a value in the audience claim.  If the principal
-     processing the claim does not identify itself with a value in the
-     "aud" claim when this claim is present, then the JWT MUST be
-     rejected. The interpretation of audience values is generally application specific.
-     The "aud" value is case-sensitive.
-     */
-    var aud: [String]? { get }
-    
-    /**
      The "exp" (expiration time) claim identifies the expiration time on
      or after which the JWT MUST NOT be accepted for processing.  The
      processing of the "exp" claim requires that the current date/time
@@ -74,33 +46,10 @@ public protocol Claims: Codable {
      */
     var iat: Date? { get }
     
-    /**
-     The "jti" (JWT ID) claim provides a unique identifier for the JWT.
-     The identifier value MUST be assigned in a manner that ensures that
-     there is a negligible probability that the same value will be
-     accidentally assigned to a different data object; if the application
-     uses multiple issuers, collisions MUST be prevented among values
-     produced by different issuers as well.  The "jti" claim can be used
-     to prevent the JWT from being replayed.  The "jti" value is case-
-     sensitive
-     */
-    var jti: String? { get }
-    
     /// Encode the Claim object as a Base64 String.
     func encode() -> String?
 }
 public extension Claims {
-    var iss: String? {
-        return nil
-    }
-    
-    var sub: String? {
-        return nil
-    }
-    
-    var aud: [String]? {
-        return nil
-    }
     
     var exp: Date? {
         return nil
@@ -111,10 +60,6 @@ public extension Claims {
     }
     
     var iat: Date? {
-        return nil
-    }
-    
-    var jti: String? {
         return nil
     }
     
