66e9814373ff9c95fea3f361f46419f596f0aa3b
David Jones <djones6@uk.ibm.com>
2019-09-24 11:23:12 +0100

    feat: Make base64url Data extensions public

--- Sources/SwiftJWT/Data+Base64URLEncoded.swift
+++ Sources/SwiftJWT/Data+Base64URLEncoded.swift
@@ -1,5 +1,5 @@
 /**
- * Copyright IBM Corporation 2017
+ * Copyright IBM Corporation 2017-2019
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,16 +16,25 @@
 
 import Foundation
 
-
+/// Convenience extensions to `Data` for converting to and from base64url encoding.
 extension Data {
-    func base64urlEncodedString() -> String {
+
+    /// Returns a `String` representation of this data, encoded in base64url format
+    /// as defined in RFC4648 (https://tools.ietf.org/html/rfc4648).
+    ///
+    /// This is the appropriate format for encoding the header and claims of a JWT.
+    public func base64urlEncodedString() -> String {
         let result = self.base64EncodedString()
         return result.replacingOccurrences(of: "+", with: "-")
             .replacingOccurrences(of: "/", with: "_")
             .replacingOccurrences(of: "=", with: "")
     }
     
-    init?(base64urlEncoded: String) {
+    /// Initializes a new `Data` from the base64url-encoded `String` provided. The
+    /// base64url encoding is defined in RFC4648 (https://tools.ietf.org/html/rfc4648).
+    ///
+    /// This is appropriate for reading the header or claims portion of a JWT string.
+    public init?(base64urlEncoded: String) {
         let paddingLength = 4 - base64urlEncoded.count % 4
         let padding = (paddingLength < 4) ? String(repeating: "=", count: paddingLength) : ""
         let base64EncodedString = base64urlEncoded
